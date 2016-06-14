46cf8c9d8e14c4355a13484dbbeea8531d33dd9a
leiyonglin <leiyonglin@gmail.com>
2016-06-14 09:21:33 +0000

    update

---
+++ src/app/components/node/node.ts
@@ -0,0 +1,22 @@
+import {Component} from '@angular/core';
+import {Http} from '@angular/http';
+
+
+@Component({
+  selector: 'node',
+  templateUrl: 'app/components/node/node.html',
+  styleUrls: ['app/components/node/node.css'],
+  providers: [],
+  directives: [],
+  pipes: []
+})
+export class Node {
+
+  constructor(http: Http) {
+
+  }
+
+  ngOnInit() {
+
+  }
+}
--- src/app/seed-app.ts
+++ src/app/seed-app.ts
@@ -5,6 +5,8 @@ import {Home} from './components/home/home';
 import {About} from './components/about/about';
 import {RepoBrowser} from './components/repo-browser/repo-browser';
 
+import {Node} from './components/node/node';
+
 @Component({
   selector: 'seed-app',
   providers: [],
@@ -16,6 +18,7 @@ import {RepoBrowser} from './components/repo-browser/repo-browser';
   { path: '/home',       component: Home,        name: 'Home', useAsDefault: true },
   { path: '/about',      component: About,       name: 'About' },
   { path: '/github/...', component: RepoBrowser, name: 'RepoBrowser' },
+  { path: '/node', component: Node, name: 'Node' },
 ])
 export class SeedApp {
 
