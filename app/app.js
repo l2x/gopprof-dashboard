80b39f3ce6a30b1f334d61aa9597314a9f91dc3d
leiyonglin <leiyonglin@gmail.com>
2016-06-27 15:35:38 +0800

    update

--- app/app.js
+++ app/app.js
@@ -3,23 +3,31 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
     'ngRoute',
-    'ngMaterial',
-    'highcharts-ng',
-    'datatables',
     'myApp.view1',
     'myApp.view2',
     'myApp.version'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-        $locationProvider.hashPrefix('!');
-
-        $routeProvider.otherwise({
-            redirectTo: '/view1'
-        });
-    }])
-    .config(function($mdThemingProvider) {
-        $mdThemingProvider.theme('default')
-            .primaryPalette('blue', {
-                'default': '700',
-            })
+    //$locationProvider.hashPrefix('!');
+    $routeProvider.otherwise({
+        redirectTo: '/view1'
     });
+}])
+.directive('activeLink', ['$location', function(location) {
+    return {
+        restrict: 'A',
+        link: function(scope, el, attrs, controller) {
+            var clazz = attrs.activeLink;
+            var path = el.children('a').attr('href');
+            path = path.substring(1);
+            scope.location = location;
+            scope.$watch('location.path()', function(newPath) {
+                if (path === newPath) {
+                    el.addClass(clazz);
+                } else {
+                    el.removeClass(clazz);
+                }
+            });
+        }
+    };
+}]);
--- app/view2/view2.js
+++ app/view2/view2.js
@@ -1,21 +1,14 @@
 'use strict';
 
 angular.module('myApp.view2', ['ngRoute'])
-    .config(['$routeProvider', function($routeProvider) {
-        $routeProvider.when('/view2', {
-            templateUrl: 'view2/view2.html',
-            controller: 'View2Ctrl'
-        });
-    }])
-    .controller('View2Ctrl', function($scope,DTOptionsBuilder, DTColumnDefBuilder) {
-      var vm = this;
-    $scope.dtOptions = DTOptionsBuilder.newOptions()
-        .withPaginationType('full_numbers')
-        .withDisplayLength(2)
-        .withDOM('pitrfl');
-    $scope.dtColumnDefs = [
-        DTColumnDefBuilder.newColumnDef(0),
-        DTColumnDefBuilder.newColumnDef(1).notVisible(),
-        DTColumnDefBuilder.newColumnDef(2).notSortable()
-    ];
-    });
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view2', {
+    templateUrl: 'view2/view2.html',
+    controller: 'View2Ctrl'
+  });
+}])
+
+.controller('View2Ctrl', [function() {
+
+}]);
