9670daaed186ae00a9ec6b1115782403cbba7652
leiyonglin <leiyonglin@gmail.com>
2016-06-27 15:56:40 +0800

    update

--- app/app.js
+++ app/app.js
@@ -1,33 +1,43 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
+var myApp = angular.module('myApp', [
     'ngRoute',
-    'myApp.view1',
-    'myApp.view2',
-    'myApp.version'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-    //$locationProvider.hashPrefix('!');
-    $routeProvider.otherwise({
-        redirectTo: '/view1'
-    });
-}])
-.directive('activeLink', ['$location', function(location) {
-    return {
-        restrict: 'A',
-        link: function(scope, el, attrs, controller) {
-            var clazz = attrs.activeLink;
-            var path = el.children('a').attr('href');
-            path = path.substring(1);
-            scope.location = location;
-            scope.$watch('location.path()', function(newPath) {
-                if (path === newPath) {
-                    el.addClass(clazz);
-                } else {
-                    el.removeClass(clazz);
-                }
+        //$locationProvider.hashPrefix('!');
+        $routeProvider
+            .when('/', {
+                templateUrl: 'pprof/index.html',
+                controller: 'PprofCtrl'
+            })
+            .when('/pprof', {
+                templateUrl: 'pprof/index.html',
+                controller: 'PprofCtrl'
+            })
+            .when('/stats', {
+                templateUrl: 'stats/index.html',
+                controller: 'StatsCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
             });
-        }
-    };
-}]);
+    }])
+    .directive('activeLink', ['$location', function(location) {
+        return {
+            restrict: 'A',
+            link: function(scope, el, attrs, controller) {
+                var clazz = attrs.activeLink;
+                var path = el.children('a').attr('href');
+                path = path.substring(1);
+                scope.location = location;
+                scope.$watch('location.path()', function(newPath) {
+                    if (path === newPath) {
+                        el.addClass(clazz);
+                    } else {
+                        el.removeClass(clazz);
+                    }
+                });
+            }
+        };
+    }]);
---
+++ app/pprof/index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+myApp.controller('PprofCtrl', function($scope) {
+    $scope.greeting = 'Hola!';
+});
